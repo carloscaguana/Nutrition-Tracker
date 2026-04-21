e75db22afbb6903caef023f9c24f90dfa1dffef5
carloscaguana <carloscaguana003@gmail.com>
2026-04-20 21:34:46 -0400

    Modified 'foodApi.ts' to be able to list all foods in the current dataset

--- nutrition-tracker-frontend/src/features/foods/foodApi.ts
+++ nutrition-tracker-frontend/src/features/foods/foodApi.ts
@@ -10,3 +10,25 @@ export const searchFoods = async (query: string) => {
   if (error) throw error
   return data
 }
+
+const PAGE_SIZE = 50
+
+// used to list all available foods in the dataset
+export const getAllFoods = async (page: number, query: string) => {
+  const from = page * PAGE_SIZE
+  const to = from + PAGE_SIZE - 1
+
+  let req = supabase
+    .from('foods')
+    .select ('*', { count: 'exact' })
+    .order('food_name', { ascending: true })
+    .range(from, to)
+
+  if (query.trim()) {
+    req = req.ilike('food_name', `%${query.trim()}%`)
+  }
+
+  const { data, error, count } = await req
+  if (error) throw error
+  return { data: data ?? [], count: count ?? 0, page_size: PAGE_SIZE}
+}
